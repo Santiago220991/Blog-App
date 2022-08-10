4c8efb9de6d3aa56879849b1291beb7628cf6903
Santiago <alexander.cardenas10@outlook.com>
2022-08-09 21:40:50 -0500

    Check linters ready to pull request

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    belongs_to :post
-    after_save :l_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_save :l_counter
 
-    def l_counter
-        id=self.post_id
-        post=Post.where(id: id)
-        post.update(likes_counter: Like.where(post_id: id).count)
-    end
+  def l_counter
+    id = post_id
+    post = Post.where(id:)
+    post.update(likes_counter: Like.where(post_id: id).count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :likes
-    has_many :comments
-    has_many :posts
+  has_many :likes
+  has_many :comments
+  has_many :posts
 
-    def last_posts(id)
-        Post.where(author: id).order(created_at: :desc).limit(3)
-    end
+  def last_posts(id)
+    Post.where(author: id).order(created_at: :desc).limit(3)
+  end
 end
