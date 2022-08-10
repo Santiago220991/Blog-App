9b474bce4dc87bea9cbf7092de6d7ca2db4568c4
Santiago <alexander.cardenas10@outlook.com>
2022-08-09 21:37:30 -0500

    Add method to return the last 3 post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :likes
     has_many :comments
     has_many :posts
+
+    def last_posts(id)
+        Post.where(author: id).order(created_at: :desc).limit(3)
+    end
 end
