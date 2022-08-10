7e8aa0ce3e1786eb0bd2b24b0aea3c44b5d999ac
Santiago <alexander.cardenas10@outlook.com>
2022-08-10 13:02:50 -0500

    Fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :likes 
   has_many :comments
   has_many :posts
-  validates :name, presence: true, allow_blank: false
-  validates :posts_counter, comparision: {greater_than_or_equal_to: 0}, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :posts_counter, numericality: {greater_than_or_equal_to: 0, only_integer: true}
   def last_posts(id)
     Post.where(author: id).order(created_at: :desc).limit(3)
   end
