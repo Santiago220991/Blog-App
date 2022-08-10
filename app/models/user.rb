5651da3a5b68250b961ef67b0003b9df24c100ff
Santiago <alexander.cardenas10@outlook.com>
2022-08-10 12:54:23 -0500

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_many :likes
+  has_many :likes 
   has_many :comments
   has_many :posts
-
+  validates :name, presence: true, allow_blank: false
+  validates :posts_counter, comparision: {greater_than_or_equal_to: 0}, numericality: { only_integer: true }
   def last_posts(id)
     Post.where(author: id).order(created_at: :desc).limit(3)
   end
