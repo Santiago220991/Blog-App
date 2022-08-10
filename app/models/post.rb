8b67f0a99a27fcfbee2714cc8a6ddb23555a843e
Santiago <alexander.cardenas10@outlook.com>
2022-08-09 21:38:52 -0500

    Add method to update post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: "User"
     has_many :likes
     has_many :comments
+    after_save :p_counter
+
+    def p_counter
+        id=self.author_id
+        user=User.find(id)
+        user.update(posts_counter: Post.where(author: id).count)
+    end
+
+    def last_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
