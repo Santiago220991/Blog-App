bc3f5a9a7149ef681a08c13ab5421e9c368ec16e
Santiago <alexander.cardenas10@outlook.com>
2022-08-15 18:04:17 -0500

    Modify root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/users', to: "users#index", as: :users
   get '/users/:user_id/posts/:id', to: "posts#show", as: :user_post
   get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
+  root to:'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
