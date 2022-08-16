78c7805e6df28ac89a134e7af91a16f5c1b515b3
Santiago <alexander.cardenas10@outlook.com>
2022-08-15 19:15:17 -0500

    Fix feature to show proper posts's owner

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ActionController::Base
     end
 
     def show
-        @post=Post.find(params[:id])
+        @post=Post.where(author_id: params[:user_id], id: params[:id]).first
         @post_num=Post.where(author_id: @post.author_id).index{|post| post.id==params[:id].to_i}
         @comments=Comment.where(post_id: params[:id])
         @user=User.find(@post.author_id)
