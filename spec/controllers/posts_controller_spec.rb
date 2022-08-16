c54f7b829206a4ab2cba21a175bc608c6612f4cd
Santiago <alexander.cardenas10@outlook.com>
2022-08-16 11:21:33 -0500

    Add test to posts_controller

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "testing posts_controller", :type => :request do
+  context " testing posts#index" do
+   it "return a http status 200" do
+        get "/users/1/posts"
+        expect(response).to have_http_status(200)
+   end
+
+   it "should render index view" do
+        get "/users/1/posts"
+        expect(response).to render_template('index')
+   end
+
+   it "render should have a posts_container class" do
+
+        get "/users/1/posts"
+        expect(response.body).to include('posts_title')
+   end
+end
+
+context "testing posts#show" do
+   it "return a http status 200" do
+    get "/users/1/posts/1"
+    expect(response).to have_http_status(200)
+   end
+
+   it "should render index view" do
+        get "/users/1/posts/1"
+        expect(response).to render_template('show')
+    end
+
+   it "render should have a posts_container class" do
+        get "/users/1/posts/1"
+        expect(response.body).to include('post_title')
+    end
+end
+end
