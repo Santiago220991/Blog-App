75da96d00775b6a3a5787d0b0f7ab5f8b5061e4d
Santiago <alexander.cardenas10@outlook.com>
2022-08-15 19:59:44 -0500

    Fix issue to show the number of post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ActionController::Base
-    def index
-        @user=User.find(params[:user_id])
-        @user_posts=Post.where(author_id: params[:user_id])
-        @comments=Comment.all
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @user_posts = Post.where(author_id: params[:user_id])
+    @comments = Comment.all
+  end
 
-    def show
-        @post=Post.where(author_id: params[:user_id], id: params[:id]).first
-        @post_num=Post.where(author_id: @post.author_id).index{|post| post.id==params[:id].to_i}
-        @comments=Comment.where(post_id: params[:id])
-        @user=User.find(@post.author_id)
-    end
+  def show
+    @post = (Post.where(author_id: params[:user_id]).order(created_at: :asc))[(params[:id].to_i)-1]
+    @comments = Comment.where(post_id: params[:id])
+    @user = User.find(@post.author_id)
+  end
 end
