7064556bd752fad72d64ecbe909ec2365df66ba3
Santiago <alexander.cardenas10@outlook.com>
2022-08-16 22:50:30 -0500

    Check linters ready to pull request

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   def last_posts
-    Post.where(author: self.id).order(created_at: :desc).limit(3)
+    Post.where(author: id).order(created_at: :desc).limit(3)
   end
 end
