0a6a8938d2a1e55dedf1295f00fabf7416663da5
Santiago <alexander.cardenas10@outlook.com>
2022-08-18 11:52:23 -0500

    Check linters ready to pull request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,16 @@ class PostsController < ApplicationController
     @comments = Comment.where(post_id: params[:id])
     @user = User.find(@post.author_id)
   end
-  
+
   def new
-    @post=Post.new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.author_id=self.current_user.id
-    @post.comments_counter=0
-    @post.likes_counter=0
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
     if @post.save
       redirect_to user_posts_path(@post.author_id)
     else
