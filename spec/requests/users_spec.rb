94bcb427a50e364d7c175a4aa4b498c2dd8b24ab
Santiago <alexander.cardenas10@outlook.com>
2022-08-22 22:00:55 -0500

    Refactor users request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'testing users_controller', type: :request do
+  before :each do
+    Comment.destroy_all
+    Like.destroy_all
+    Post.destroy_all
+    User.destroy_all
+    @user = User.create(name: 'Tom', photo: 'https://unsp', bio: 'Teacher from Mexico.', posts_counter: 0)
+    @post = Post.create(author: @user, title: 'Hello', text: 'Post', comments_counter: 0, likes_counter: 0)
+    @post.save
+  end
   context ' testing users#index' do
     it 'return a http status 200' do
       get '/users'
@@ -20,17 +29,17 @@ RSpec.describe 'testing users_controller', type: :request do
 
   context 'testing users#show' do
     it 'return a http status 200' do
-      get '/users/1'
+      get "/users/#{@user.id}"
       expect(response).to have_http_status(200)
     end
 
     it 'should render index view' do
-      get '/users/1'
+      get "/users/#{@user.id}"
       expect(response).to render_template('show')
     end
 
     it 'render should have a users_container class' do
-      get '/users/1'
+      get "/users/#{@user.id}"
       expect(response.body).to include('user_title')
     end
   end
