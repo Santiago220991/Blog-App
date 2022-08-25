7c326bd1b3b439a3c8526f468fb370d7c8708afa
HectorTorresE <hectorjte@gmail.com>
2022-08-25 09:43:44 -0600

    Add destroy method to comment controlelr

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:post_id])
+    if Comment.destroy(params[:id])
+      redirect_to user_post_path(post.author_id, post.id)
+    end
+  end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
