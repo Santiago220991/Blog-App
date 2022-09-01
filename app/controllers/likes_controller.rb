cf7e9bae952a29f89215cb3c5b9d9fd043ef6ec5
Santiago <alexander.cardenas10@outlook.com>
2022-09-01 13:15:15 -0500

    Fix add like issue

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,6 +7,8 @@ class LikesController < ApplicationController
     @like = Like.new
     @like.author_id = current_user.id
     @like.post_id = params[:post_id]
-    redirect_to user_posts_path(:author_id) if @like.save
+     if @like.save
+      redirect_back(fallback_location: root_path)
+     end
   end
 end
