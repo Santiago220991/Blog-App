0af77e66165561cc4be46220a30a203d6f051623
Santiago <alexander.cardenas10@outlook.com>
2022-09-02 15:03:47 -0500

    Add feature to update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_posts = @user.last_posts
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user= User.find(params[:id])
+    redirect_to user_path(params[:id]) if @user.update(bio: params[:user][:bio], photo: params[:user][:photo] )
+  end
 end
